48e4943bf844c07cddd2c9a4709e97dca54fd988
ikbalrahmat <ikbalrahmattaupil26@gmail.com>
2024-06-07 18:22:38 +0700

    Develop delete todo feature

--- src/component/TodoItem.jsx
+++ src/component/TodoItem.jsx
@@ -17,23 +17,35 @@ const TodoItem = ({ todo, toggleCompleted }) => {
         onChange={() => toggleCompleted(todo.id)}
       />
       <p style={getTodoTitleStyle()}>{todo.title}</p>
+      {/* Tambahkan sebuah button di sini */}
+      <button style={styles.button}>x</button>
     </div>
-  );
-};
+  )
+}
 
 const styles = {
   todoItem: {
     border: '2px solid #f4f4f4',
     fontSize: '24px',
     display: 'flex',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
     alignItems: 'center',
+    padding: '0 20px',
   },
   checkbox: {
-    marginRight: '10px',
     height: '18px',
     width: '18px',
   },
-};
+  button: {
+    backgroundColor: '#BB0000',
+    color: '#fff',
+    height: '30px',
+    width: '30px',
+    borderRadius: '100%',
+    border: 'none',
+    cursor: 'pointer',
+    fontSize: '16px',
+  },
+}
 
 export default TodoItem;
